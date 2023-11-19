30e1641f0dca930bd66baaf6879746386668fff9
anton.bashirov <anton.sh.local@gmail.com>
2023-11-19 14:14:33 +0200

    reload 3.2.0

--- reload.sh
+++ reload.sh
@@ -2,13 +2,15 @@
 
 VERSION=3.2.0
 
-rm -rf target
+shopt -s extglob
+rm -rf * !(reload.sh)
 mkdir -p target
 cd target
 wget -q https://github.com/dart-lang/sdk/archive/refs/tags/$VERSION.zip
 unzip -q $VERSION.zip
 cp -r sdk-$VERSION/runtime/include/* ../
 cd .. && rm -rf target
+rm -rf BUILD.gn
 git tag $VERSION
 git add .
 git commit -am "reload $VERSION"
